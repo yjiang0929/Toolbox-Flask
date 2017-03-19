4b459967548ace348e4ff68be52170744859d52c
Yichen Jiang <pauljyc0929@gmail.com>
2017-03-18 23:26:32 -0400

    Turn in Toolbox-flask

--- flask_app.py
+++ flask_app.py
@@ -1,3 +1,26 @@
 """
 Put your Flask app code here.
 """
+
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return render_template('hello.html')
+
+@app.route('/login/', methods=['GETS','POST'])
+def login():
+    error = None
+    if request.method == "POST":
+        name = request.form.get('name','')
+        age = request.form.get('age','')
+        ninja = request.form.get('ninja','')
+        if name == '' or age == '' or ninja == '':
+            error = 'Please check your input and fill in all the boxes'
+    return render_template('login.html',name=name, age=age, ninja = 'Patrick Huston',
+                           error=error)
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
